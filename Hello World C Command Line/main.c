d9e20825b198d49f132f3a2981e45bff8328d717
daniel-lyons <daniellyons@Daniel-Lyons-Computer.local>
2013-10-12 05:11:28 -1000

    Initial Commit

---
+++ Hello World C Command Line/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello World C Command Line
+//
+//  Created by Daniel Lyons on 10/12/13.
+//  Copyright (c) 2013 Daniel Lyons. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
